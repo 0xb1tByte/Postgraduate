fdfac62d3df7df76094a68158c448e4e40cb5b8e
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2019-12-01 23:22:17 +0000

    Update ModularExponentiation.java

--- Cryptography/Assignments/Modular Exponentiation/ModularExponentiation.java
+++ Cryptography/Assignments/Modular Exponentiation/ModularExponentiation.java
@@ -1,21 +1,59 @@
+package modularexponentiation;
 
-package testfortest;
+import java.math.BigInteger;
 
 /**
  *
  * @author Alaa
  */
 public class ModularExponentiation {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        int base = 5;
-        int exponent = 0;
-        int modulo = 23;
-        long result;
-        for (;exponent < modulo ; exponent++)
-        {
-         result = (long) Math.pow(base, exponent) % modulo; 
-         System.out.print(base + " ^ " + exponent + " % " + modulo);
-         System.out.println(" = " + result);
+        // Test 
+        int baseSmall = 5;
+        int exponentSmall = 2;
+        int moduloSmall = 23;
+        System.out.println("Modular Exponent for small numbers using Math.pow");
+        ModularExponent(baseSmall,exponentSmall,moduloSmall);
+        
+        BigInteger baseLarge = new BigInteger("30");
+        BigInteger exponentLarge = new BigInteger("13");
+        BigInteger moduloLarge = new BigInteger("79");
+        System.out.println("Modular Exponent for large numbers using modPow");
+        ModularExponentForBigInteger(baseLarge,exponentLarge,moduloLarge);
+    }
+
+    // Works for large base,exponent, and modulo
+    private static BigInteger ModularExponentForBigInteger(BigInteger base, BigInteger exponent, BigInteger modulo) {
+        BigInteger result = base.modPow(exponent, modulo);
+        System.out.print(base + " ^ " + exponent + " % " + modulo);
+        System.out.println(" = " + result);
+        return result;
+    } // end ModularExponentForBigInteger()
+
+    // Works only when the number of digits (results from Math.pw < 18)
+    private static long ModularExponent(long base, long exponent, long modulo) {
+        long result = 0;
+        result = (long) Math.pow(base, exponent) % modulo;
+        System.out.print(base + " ^ " + exponent + " % " + modulo);
+        System.out.println(" = " +result);
+        return result;
+    } // end ModularExponent()
+
+    // Works only when the number of digits (results from Math.pw < 18)
+    private static void ModularExponentLoop(long base, long exponent, long modulo) {
+        long result = 0;
+        long power = 0;
+
+        for (; exponent < modulo; exponent++) {
+            power = (long) Math.pow(base, exponent);
+            System.out.println(base + " ^ " + exponent + " = " + power);
+            result = (long) Math.pow(base, exponent) % modulo;
+            System.out.print(base + " ^ " + exponent + " % " + modulo);
+            System.out.println(" = " + result);
         } // end for  
-    } // end main
-} // end class
+    } // end ModularExponentLoop()
+}
